2dd5ead27777ca50070cbb0e2e36a12fde597c4e
Miya <qianwumeng_2cyndi@qq.com>
2020-05-16 01:24:29 +0800

    Update About.vue

--- src/src/views/About.vue
+++ src/src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="About">
+    About
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component({
+  // 组件注册
+  components: {
+
+}})
+export default class About extends Vue {
+// data
+
+// props
+
+// create
+private created() {
+
+}
+
+// mounted
+private mounted() {
+// TODO
+}
+
+}
+</script>
+<style lang="less">
+
+</style>
