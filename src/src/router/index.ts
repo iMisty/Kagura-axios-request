0c647e7fec9999f8e09fa57a386609a8bc8c851f
Miya <qianwumeng_2cyndi@qq.com>
2020-05-19 01:24:47 +0800

    Update index.ts

--- src/src/router/index.ts
+++ src/src/router/index.ts
@@ -13,10 +13,17 @@ const routes: RouteConfig[] = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/archives',
+    name: 'Archives',
+    component: () => import('../views/Archives.vue'),
+  },
+  {
+    path: '/game',
+    name: 'Game',
+    component: () => import('../views/Game.vue'),
   },
 ];
 
