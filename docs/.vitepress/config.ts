acce79d4029ad9520979284811a33e239c8dbe7e
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-08-20 03:10:32 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2022-08-20 01:27:54
- * @LastEditTime: 2022-08-20 03:04:29
+ * @LastEditTime: 2022-08-20 03:10:23
  * @LastEditors: Miya
  * @Description: VuePress Config Root
  * @FilePath: \undefinedf:\Documents\Github\Kagura-axios-request\docs\.vitepress\config.ts
@@ -13,7 +13,7 @@ import configSidebar from './components/sidebar';
 export default {
   title: 'Miramiya/Request',
   description: 'Just playing around.',
-  base:'/kagura-axios-request',
+  base:'/kagura-axios-request/',
   themeConfig: {
     nav: configNavbar,
     sidebar: configSidebar,
